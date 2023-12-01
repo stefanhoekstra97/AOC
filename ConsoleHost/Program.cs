ef3858543e548ff66fcf721ba4b8e63a45bcf571
Stefan Hoekstra <stefan.hoekstra@infosupport.com>
2023-12-01 15:14:18 +0100

    init

---
+++ ConsoleHost/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Infrastructure;
+using PuzzleSolving;
+
+var input = await ReadFile.ReadFromDisk("puzzle_input", 1);
+// var input = await ReadFile.ReadFromDisk("sample_hard", 1);
+var solver = new CalibrateLaunch();
+
+// var solutionEasy = solver.SolveEasy(input.Input);
+
+var solutionHard = solver.SolveHard(input.Input);
+// Console.WriteLine($"Easy: {solutionEasy}");
+Console.WriteLine($"Hard: {solutionHard}");
