506a1c4a4edbc120e17a10abb7613cff22f2ffa0
Stefan <stefanhoekstra.97@gmail.com>
2023-12-03 08:25:48 +0100

    Day two, with bench and regex version

---
+++ Benchmarks/DayTwoBenchmarks/CubeGameBenchmarks.cs
@@ -0,0 +1,55 @@
+using Infrastructure.Entities;
+using PuzzleSolving._02_CubeGame;
+
+namespace Benchmarks.DayTwoBenchmarks;
+
+using BenchmarkDotNet.Attributes;
+using Infrastructure;
+using PuzzleSolving;
+
+public class CubeGameBenchmarks
+{
+    private PuzzleInput _input;
+    private CubeGame _solver;
+
+    [GlobalSetup]
+    public async void GlobalSetup()
+    {
+        var puzzleInput = await ReadFile.ReadFromDisk("puzzle_input", 2);
+        _input = puzzleInput;
+
+        _solver = new CubeGame();
+    }
+
+    [Benchmark]
+    public void SolveEasy()
+    {
+        _solver.SolveEasy(_input);
+    }
+
+    [Benchmark]
+    public void SolveEasyRegex()
+    {
+        _solver.SolveEasyRegex(_input);
+    }
+
+    [Benchmark]
+    public void SolveSecondCubeBench()
+    {
+        _solver.SolveHard(_input);
+    }
+
+    [Benchmark]
+    public async Task SolveSecondCubeBenchIncReadInput()
+    {
+        var puzzleInput = await ReadFile.ReadFromDisk("puzzle_input", 2);
+        var inp = puzzleInput;
+
+        var solv = new CubeGame();
+        solv.SolveHard(inp);
+    }
+
+
+
+
+}
